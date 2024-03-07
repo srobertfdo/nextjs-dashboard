ee468d6ebf8fd08c74125ba0a8384c538712cb54
Robert S <rsahayar@ford.com>
2024-03-07 19:48:42 +0530

    Search fixes

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,13 +6,13 @@ import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
-    const pathname = usePathname();
-    const { replace } = useRouter();
 
     if (term) {
       params.set('query', term);
