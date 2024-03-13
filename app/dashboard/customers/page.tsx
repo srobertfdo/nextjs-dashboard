51ad664c71da75490e15d13c684f8860c799c295
Robert S <rsahayar@ford.com>
2024-03-13 12:23:34 +0530

    Chapter 16: Adding Meta data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,10 @@
 import dynamic from 'next/dynamic';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 const DynamicClientComponent = dynamic(() => import('@/app/ui/customers/table'), {
     ssr: true,
 });
