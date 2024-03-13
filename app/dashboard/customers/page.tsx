f420310b65cf4255de17552c6662c5b0d76fce6c
Robert S <rsahayar@ford.com>
2024-03-13 12:27:36 +0530

    customers page initial

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,19 +1,14 @@
-import dynamic from 'next/dynamic';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-const DynamicClientComponent = dynamic(() => import('@/app/ui/customers/table'), {
-    ssr: true,
-});
-
 export default function Page() {
     
     return (
         <div>
-            <DynamicClientComponent/>
+            <p>Customers</p>
         </div>
     );
 }
