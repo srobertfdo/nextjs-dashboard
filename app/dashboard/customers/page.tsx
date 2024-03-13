bb33b59caf9746dfba691ede71c32726446ed0b0
Robert S <rsahayar@ford.com>
2024-03-13 15:32:28 +0530

    Fixing GO code

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,14 +1,19 @@
+import dynamic from 'next/dynamic';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
+const DynamicClientComponent = dynamic(() => import('@/app/ui/customers/table'), {
+    ssr: true,
+});
+
 export default function Page() {
     
     return (
         <div>
-            <p>Customers</p>
+            <DynamicClientComponent/>
         </div>
     );
 }
